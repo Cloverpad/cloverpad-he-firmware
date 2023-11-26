314ec0358042b9e72bda14951999b555f405f09f
Jon Pacheco <jon.pacheco@tutanota.com>
2023-11-26 18:03:34 +0000

    chore: setup firmware project

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
+#include <Keyboard.h>
 
-// put function declarations here:
-int myFunction(int, int);
+/// @brief Setup code for Core 0.
+void setup()
+{
+    Keyboard.begin();
+    Serial.begin();
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // Signal to Core 1 that it's finished setting up
+    rp2040.fifo.push(1);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+/// @brief Setup code for Core 1.
+void setup1()
+{
+    // Wait for Core 0 to finish setting up
+    rp2040.fifo.pop();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+/// @brief Main loop for Core 0. Used to handle serial communication and RGB effects.
+void loop()
+{
+}
+
+/// @brief Main loop for Core 1. Used to handle reading hall effect inputs and sending HID commands.
+void loop1()
+{
 }
