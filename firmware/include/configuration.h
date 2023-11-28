54d6cbdeb043aa3d5d0c64be8dcf323162d87d04
Jon Pacheco <jon.pacheco@tutanota.com>
2023-11-28 23:57:02 +0000

    feat: add keypad name to main configuration

--- firmware/include/configuration.h
+++ firmware/include/configuration.h
@@ -45,6 +45,9 @@ struct KeypadConfiguration {
     /// @brief The configuration version.
     uint32_t configuration_version = CONFIGURATION_VERSION;
 
+    /// @brief The name of this keypad.
+    char name[64] = "Unnamed Keypad";
+
     /// @brief The configurations for each hall effect key on this keypad.
     HEKeyConfiguration he_keys[HE_KEY_COUNT] = {};
 };
