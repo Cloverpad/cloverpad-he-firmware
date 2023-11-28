4b35581e03514d46d439672e5299b890f8b990b9
Jon Pacheco <jon.pacheco@tutanota.com>
2023-11-28 16:52:18 +0000

    fix: re-build core crate if protobuf definitions change

--- configurator/core/build.rs
+++ configurator/core/build.rs
@@ -1,6 +1,10 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
+    // Re-build if protobuf defintions have changed
+    // The .options files are specific to nanopb, so no need to rebuild if they change
+    println!("cargo:rerun-if-changed=../../protocol/*.proto");
+
     prost_build::compile_protos(&["../../protocol/commands.proto"], &["../../protocol"])?;
     Ok(())
 }
