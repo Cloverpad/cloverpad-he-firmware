7eace377120ee698be029ee40f54a0800fb70b65
Jon Pacheco <jon.pacheco@tutanota.com>
2023-11-28 16:53:20 +0000

    fix: typo in build script

--- configurator/core/build.rs
+++ configurator/core/build.rs
@@ -1,7 +1,7 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    // Re-build if protobuf defintions have changed
+    // Rebuild if protobuf defintions have changed
     // The .options files are specific to nanopb, so no need to rebuild if they change
     println!("cargo:rerun-if-changed=../../protocol/*.proto");
 
