530d23a51f939192fabcca0914ba5d93dda81f88
Jon Pacheco <jon.pacheco@tutanota.com>
2023-11-29 00:30:15 +0000

    fix: actually load the default configuration on startup

--- firmware/include/main.h
+++ firmware/include/main.h
@@ -5,7 +5,7 @@
 #include <serial.h>
 
 /// @brief The top-level configuration currently in use.
-KeypadConfiguration configuration = {};
+KeypadConfiguration configuration = get_default_keypad_config();
 
 /// @brief The serial handler used by the main loop.
 SerialHandler serial_handler = SerialHandler();
