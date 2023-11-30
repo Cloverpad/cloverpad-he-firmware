9d946901b492b594f2c8a61744eabb339b4a1bfb
Jon Pacheco <jon.pacheco@tutanota.com>
2023-11-30 18:35:25 +0000

    fix: replace use of glob in build script
    doesn't work - always ends up rebuilding the core project (at least on windows)

--- configurator/core/build.rs
+++ configurator/core/build.rs
@@ -3,7 +3,12 @@ use std::io::Result;
 fn main() -> Result<()> {
     // Rebuild if protobuf defintions have changed
     // The .options files are specific to nanopb, so no need to rebuild if they change
-    println!("cargo:rerun-if-changed=../../protocol/*.proto");
+    println!("cargo:rerun-if-changed=../../protocol/protocol-commands-config.proto");
+    println!("cargo:rerun-if-changed=../../protocol/protocol-commands-main.proto");
+    println!("cargo:rerun-if-changed=../../protocol/protocol-commands-state.proto");
+    println!("cargo:rerun-if-changed=../../protocol/protocol-commands.proto");
+    println!("cargo:rerun-if-changed=../../protocol/protocol-config.proto");
+    println!("cargo:rerun-if-changed=../../protocol/protocol-state.proto");
 
     prost_build::compile_protos(&["../../protocol/protocol-commands.proto"], &["../../protocol"])?;
     Ok(())
