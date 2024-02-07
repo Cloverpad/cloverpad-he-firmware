a82c7e60dd2f2d9a8c013382176d32f2b92b12bc
Jon Pacheco <jon.pacheco2@gmail.com>
2024-02-07 23:28:04 +0000

    refactor: use clamp function for linear interpolation

--- lib/Cloverpad/include/lerp.h
+++ lib/Cloverpad/include/lerp.h
@@ -3,6 +3,8 @@
 #include <array>
 #include <cstdint>
 
+#include <utils.h>
+
 /// @brief Perform linear interpolation on the provided ADC values in software.
 /// @param ideal_adc_low The start of the ideal ADC range
 /// @param ideal_adc_range The size of the ideal ADC range
@@ -14,14 +16,7 @@ template <typename T>
 constexpr T lerp_adc(T ideal_adc_low, T ideal_adc_range, T calibrated_adc_low, T calibrated_adc_high, T value)
 {
     // Clamp the value to the calibrated range
-    if (value < calibrated_adc_low)
-    {
-        value = calibrated_adc_low;
-    }
-    else if (value > calibrated_adc_high)
-    {
-        value = calibrated_adc_high;
-    }
+    value = clamp(value, calibrated_adc_low, calibrated_adc_high);
 
     // Interpolate the ADC value, mapping it to the ideal range based on the calibration
     return ideal_adc_low + ideal_adc_range * (value - calibrated_adc_low) / (calibrated_adc_high - calibrated_adc_low);
